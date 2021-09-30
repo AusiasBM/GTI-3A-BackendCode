a0b86bdf3afbcc6ee115dd7f2d4ae53141b7aa47
Ausias <ausiasbm@gmail.com>
2021-09-30 08:43:50 +0200

    controladores básicos

--- src/rutas/mediciones.rutas.js
+++ src/rutas/mediciones.rutas.js
@@ -1,40 +1,11 @@
 // importamos solo las rutas de la librería express
 const {Router} = require("express");
+const ctrMediciones = require("../controladores/mediciones.controlador");
 const rutasMediciones = Router();
 
-rutasMediciones.get('/todas-las-mediciones', (req, res) => {
-    const mediciones = [
-        {
-            "medicion"      : "2.5",
-            "tipoMedicion"  : "Calidad del aire",
-            "fecha"         : "29-09-2021",
-            "hora"          : "12:15",
-            "lat"           : "38.99600901262704",
-            "lng"          : "-0.16582290057630056"
-        },
-        {
-            "medicion"      : "2.0",
-            "tipoMedicion"  : "Calidad del aire",
-            "fecha"         : "30-09-2021",
-            "hora"          : "09:00",
-            "lat"           : "38.99600901262704",
-            "lng"          : "-0.16582290057630056"
-        },
-    ]
-    res.json(mediciones);
-});
+rutasMediciones.get('/todas-las-mediciones', ctrMediciones.obtenerTodas);
 
-rutasMediciones.post('/anyadir-medicion', ( req, res ) => {
-
-    // Si creamos una lista con el mismo nombre que las clables del json, se añaden los valores automáticamente a cada varaible.
-    const { medicion, tipoMedicion, fecha, hora, lat, lng } = req.body;
-    console.log(medicion + ", " + tipoMedicion + ", " + fecha);
-    
-    res.json({
-        msj: "Medición recibida"
-    });
-
-});
+rutasMediciones.post('/anyadir-medicion', ctrMediciones.insertarMedicion);
 
 
 module.exports = rutasMediciones; // Exportamos todas las rutas
