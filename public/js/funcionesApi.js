0fb459a9a0d4769ea1d0c42bc8baf92343f52bf9
Ausias <ausiasbm@gmail.com>
2021-10-01 13:05:43 +0200

    web mostrando medidas
    Falta ver como mostrar la medición, ya que al ser un double no lo interpreta como tal.

---
+++ public/js/funcionesApi.js
@@ -0,0 +1,54 @@
+
+const IP_PUERTO="http://localhost:3500"
+
+function todasLasMedidiones(){
+
+    fetch(IP_PUERTO + '/api/todas-las-mediciones', {
+        method : 'get',
+        
+    }).then(function (respuesta) {
+        if(respuesta.status != 200){
+            document.getElementById('listaMedicionesEnHtml').innerHTML = `
+        
+                    <h6>Algo no ha salido bien</h6>`
+        }
+        return respuesta.json();
+    }).then((res) => {
+        console.log(res);
+        
+        listaRespJson = res;
+        num = 0;
+
+        listaRespJson.forEach(element => {
+
+            let medicion = Number.parseFloat(element.medicion.numberDecimal);
+
+            document.getElementById('listaMedicionesEnHtml').innerHTML += `
+
+                <div class="card border-primary mb-3" style="max-width: 200rem;">
+                    <div class="card-header">Medición ${num}</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Id: ${element._id}</h4>
+                        <p> 
+                            Medicion = ${medicion}
+                            <br>
+                            Tipo de medición = ${element.tipoMedicion}
+                            <br>
+                            Fecha = ${element.fecha}
+                            <br>
+                            Latitud = ${element.lat}
+                            <br>
+                            Longitud = ${element.lng}
+                            <br>
+                        </p>
+                    </div>
+                </div>
+            
+            `
+            num++;
+        });
+
+
+
+    })
+}
